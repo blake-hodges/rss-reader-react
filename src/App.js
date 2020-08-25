456f32a550154bc4c5558021423ef2be64566eb8
blake-hodges <tbhodges@gmail.com>
2020-08-25 10:54:54 -0400

    add feedwrapper with sample entries with dummy text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import Entry from './Entry';
+import React, { Component } from 'react';
+import FeedWrapper from './FeedWrapper'
 import './App.css';
 
-function App() {
-  return (
-    <div className="container-fluid mt-3">
-        <div className="row">
-            <div className="col-12 text-center">
-                <h1>RSS Feed Reader</h1>
-            </div>
-        </div>
-        <div className="row justify-content-center">
-            <div className="col-lg-6  col-md-8 col-sm-10 col-12">
-                <div className="feed-wrapper">
-                    <Entry />
+class App extends Component {
+
+    render() {
+        return (
+            <div className="container-fluid mt-3">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h1>RSS Feed Reader</h1>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-lg-6  col-md-8 col-sm-10 col-12">
+                        <div className="feed-wrapper">
+                            <FeedWrapper />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-
-  );
+        );
+    }
 }
 
 export default App;
--- src/Entry.js
+++ src/Entry.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-function Entry() {
+function Entry(props) {
     return (
         <div className="feed-entry">
-            <h5 className="entry-title">The Post Title</h5>
-            <span className="entry-author">Blake Hodges</span>
-            <p className="entry-content">lalks jasdkj kadkl jadlk fjakld fkadflkja dlkjasdkljf akjdkas</p>
-            <p className="entry-date">May 7, 1900</p>
-            <a className="entry-link" href="#">Read Post</a>
+            <h5 className="entry-title">{props.title}</h5>
+            <span className="entry-author">{props.author}</span>
+            <p className="entry-content">{props.content}</p>
+            <p className="entry-date">{props.date}</p>
+            <a className="entry-link" href={props.link}>Read Post</a>
         </div>
     )
 }
